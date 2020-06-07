831836b9777daba1bf5153e8f166e0e9a5880969
Kenny <Kenneth.mckee@protonmail.com>
2020-06-07 13:57:17 +0100

    Updated to add filter and search on output results using JQuery and Datatables.
    Signed-off-by: Kenny <Kenneth.mckee@protonmail.com>

--- app/businesslogic/OutputFormat.py
+++ app/businesslogic/OutputFormat.py
@@ -1,4 +1,7 @@
 from app.businesslogic.DecayChain import Generator
+from app.businesslogic.MeasurementUnit import Time
+from pint import UnitRegistry
+ureg = UnitRegistry()
 
 ## This function formats the results into a dictionary {nuclide : [activity, activity unit, HL, HL unit]}
 ## This the format that flask needs to get this to run.
@@ -8,12 +11,14 @@ def OutputFormat(bateman_dict, user_aunit):
     output_result = {}
     for nuclide_name, f_nuc_conc in bateman_dict.items():
         nuclidess = chain_generator.nuclides_dict.get(nuclide_name)
-
-
+        x = nuclidess.halflife.quantity
+        x.ito(ureg.month)
+        print(x)
         other_data = [format(f_nuc_conc, '.4g'),  ## format(***, '4g') reduces down the significant figures of the result
                       user_aunit,
                       nuclidess.halflife.value,
-                      nuclidess.halflife.unit
+                      nuclidess.halflife.unit,
+                      x.magnitude
                       ]
 
         resultss = {nuclide_name: other_data}
